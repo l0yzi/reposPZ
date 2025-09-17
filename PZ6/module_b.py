28db7da8698e0202e2ad116e246e7c40d7fe5256
Sm1le143 <164458474+Sm1le143@users.noreply.github.com>
2025-09-17 10:15:22 +0300

    добавил ошибку в модуле б

--- PZ6/module_b.py
+++ PZ6/module_b.py
@@ -1,8 +1,12 @@
 def process_data(value):
+я для обработки данных - ВЕРСИЯ С БАГОМ
     if not isinstance(value, (int, float)):
         raise TypeError("Значение должно быть числом")
     
     if value < 0:
         raise ValueError("Значение не может быть отрицательным")
+    
+    if value == 5:  # ошибка
+        return value / 0
     
     return value * 2
